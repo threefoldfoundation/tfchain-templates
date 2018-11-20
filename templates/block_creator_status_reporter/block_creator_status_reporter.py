ff9a56847c41c824f92d2d0b11aa06fb809999e5
LeeSmet <lee.smet@hotmail.com>
2018-11-20 16:57:12 +0100

    Fix repo layout

---
+++ templates/block_creator_status_reporter/block_creator_status_reporter.py
@@ -0,0 +1,107 @@
+import os
+import time
+import requests
+
+from zerorobot.service_collection import ServiceNotFoundError
+from zerorobot.template.base import TemplateBase
+from zerorobot.template.decorator import retry
+from zerorobot.template.state import StateCheckError
+from zerorobot.template.decorator import timeout
+
+
+class BlockCreatorStatusReporter(TemplateBase):
+    version = '0.0.1'
+    template_name = 'block_creator_status_reporter'
+
+    def __init__(self, name=None, guid=None, data=None):
+        super().__init__(name=name, guid=guid, data=data)
+        self._node_ = None
+        self._block_creator_ = None
+        self._url_ = None
+        self.recurring_action('_monitor', 300)  # every 5 minutes
+
+    def validate(self):
+        for prop in ('postUrlTemplate', 'blockCreatorIdentifier'):
+            value = self.data[prop]
+            if not value or not value.strip():
+                raise ValueError("%s must be set!" % prop)
+
+    @property
+    def _block_creator(self):
+        if not self._block_creator_:
+            try:
+                self._block_creator_ = self.api.services.get(template_uid='github.com/threefoldtoken/0-templates/block_creator/0.0.1', name=self.data['blockCreator'])
+            except ServiceNotFoundError:
+                pass
+        return self._block_creator_
+
+    @property
+    def _url(self):
+        if not self._url_:
+            self._url_ = self.data['postUrlTemplate'].format(block_creator_identifier=self.data['blockCreatorIdentifier'])
+        return self._url_
+
+    @property
+    def _node(self):
+        if not self._node_:
+            try:
+                self._node_ = self.api.services.get(template_uid='github.com/threefoldtech/0-templates/node/0.0.1', name='local')
+            except ServiceNotFoundError:
+                pass
+        return self._node_
+
+    def start(self):
+        self.state.set('status', 'running', 'ok')
+
+    def stop(self):
+        self.state.delete('status', 'running')
+
+    @timeout(150, error_message='Monitor function call timed out')
+    def _monitor(self):
+        try:
+            self.state.check('status', 'running', 'ok')
+        except StateCheckError:
+            return
+
+        self.logger.info("monitor block reporter")
+
+        if self._block_creator:
+            self.logger.info("start gathering stats about block creator")
+            report_task = self._block_creator.schedule_action('report')
+        if self._node:
+            self.logger.info("start gathering stats about node")
+            stats_task = self._node.schedule_action('stats')
+            self.logger.info("start gathering info about node")
+            info_task = self._node.schedule_action('info')
+
+        payload = dict()
+
+        if self._block_creator:
+            # Gather chain info
+            report_task.wait()
+            if report_task.state == 'ok':
+                self.logger.info("stats about of block creator received")
+                payload["chain_status"] = report_task.result
+            else:
+                self.logger.error("error during stats gathering of block creator")
+                payload["chain_status"] = {'wallet_status': 'error'}
+
+        if self._node:
+            # Gather stats info
+            self.logger.info("stats about of node received")
+            stats_task.wait()
+            if stats_task.state == 'ok':
+                stats = dict()
+                payload['stats'] = stats
+                for stat_kind, stat in stats_task.result.items():
+                    stats[stat_kind] = stat['history']['300']
+
+            # Gather node info
+            info_task.wait()
+            self.logger.info("info about of node received")
+            if info_task.state == 'ok':
+                payload['info'] = info_task.result
+
+        self.logger.info("pushing data to influxdb")
+        headers = {'content-type': 'application/json'}
+        requests.request('PUT', self._url, json=payload, headers=headers)
